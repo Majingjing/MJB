6459b25ffb290d71db5c58c783d96c930e5ecb09
Your Name <you@example.com>
2016-08-13 10:26:37 +0800

    首次提交

---
+++ cece/cece/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cece
+//
+//  Created by majing on 16/8/13.
+//  Copyright © 2016年 majing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cece/cece/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cece
+//
+//  Created by majing on 16/8/13.
+//  Copyright © 2016年 majing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
